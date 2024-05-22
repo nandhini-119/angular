270a3bed22b9d7122b67dffab47514c037203132
nandhini <nandhini1206200@gmail.com>
2024-05-22 09:08:03 +0530

    routing modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { Page2Component } from './page2/page2.component';
+import { ChildComponent } from './child/child.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:"about",
+  component:Page2Component
+},{
+  path:"",
+  component:AppComponent
+},{
+  path:"child",
+  component:ChildComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { ChildComponent } from './child/child.component';
+import { Page2Component } from './page2/page2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChildComponent
+    ChildComponent,
+    Page2Component
   ],
   imports: [
     BrowserModule,
